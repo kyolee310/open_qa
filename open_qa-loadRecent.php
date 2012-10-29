d03dee9617783751d46c224f3f389088d98308d3
EC2 Default User <ec2-user@ip-10-248-33-157.us-west-2.compute.internal>
2012-10-28 23:07:12 -0700

    added message to explain missing test records

--- open_qa-loadRecent.php
+++ open_qa-loadRecent.php
@@ -23,6 +23,8 @@
 
 	print_page_header_this_date($this_test_date);
 
+	$exists_record = 0;
+
 	foreach( $test_array as $this_test ){
 
 		if( preg_match("/^\[(\d+\-\d+\-\d+)\s+(.+)\]\s+TESTNAME\s(\S+)\sUID\s(\d+)\sSEQUENCE\s(\S+)\sOS\s(\S+\s\S+)\sGIT_HASH\s(\S+)\sTEST_STAGE\s(\S+)/" , $this_test, $match) ){
@@ -44,10 +46,20 @@
 
 			###	CREATE NEW LINE FOR NEW DATE
 			if( $matched_test_date != $this_test_date ){
+
+				if( $exists_record == 0 ){
+					print "<font size=\"2\" color=\"orange\">";
+					print "&nbspTEST RESULTS ON $this_test_date WILL BE POSTED WITHIN 12 HRS.<br>";
+					print "</font>";
+				};
+
 				$this_test_date = $matched_test_date;
 
 				print_page_header_this_date($this_test_date);
-
+				
+				$exists_record = 0;
+			}else{
+				$exists_record = 1;
 			};
 
 			print_test_result_well($matched_test_date, $matched_test_time, $matched_testname, $matched_uid, $matched_sequence, $matched_os, $matched_git_hash, $matched_test_stage, $matched_user, $matched_test_date);
